368cc18021d4e3796ebc120636af7facbc67af0d
james <>
2026-04-23 14:34:38 +0800

    1. 新增人事、寺廟頁面

--- src/main/java/tw/org/il/dongsheng/templeapp/StaffController.java
+++ src/main/java/tw/org/il/dongsheng/templeapp/StaffController.java
@@ -1,4 +1,28 @@
 package tw.org.il.dongsheng.templeapp;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import tw.org.il.dongsheng.templeapp.util.AreaUtil;
+
+import java.util.List;
+import java.util.Map;
+
 public class StaffController {
+
+    @FXML
+    private ComboBox<String> genderBox, cityBox, distBox, mailBox;
+
+    @FXML
+    public void initialize() {
+
+        Map<String, List<String>> areaMap = AreaUtil.getAllTaiwanAreas();
+        cityBox.getItems().addAll(areaMap.keySet());
+        cityBox.valueProperty().addListener((obs, oldVal, newVal) -> {
+            if (newVal != null) {
+                distBox.getItems().clear();
+                distBox.getItems().addAll(areaMap.get(newVal));
+                distBox.getSelectionModel().selectFirst();
+            }
+        });
+    }
 }
